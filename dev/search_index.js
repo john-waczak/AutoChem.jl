27653a0ba2a48b6f8fbe9a8a9bae9be6d6cbc203
Documenter.jl <documenter@juliadocs.github.io>
2023-06-29 20:27:07 +0000

    build based on 75a29f3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AutoChem","category":"page"},{"location":"#AutoChem","page":"Home","title":"AutoChem","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AutoChem.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AutoChem]","category":"page"}]
+}
