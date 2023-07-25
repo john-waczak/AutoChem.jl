4112f9426e9ee7694631d2d77e1ecf05b3637754
John Waczak <john.louis.waczak@gmail.com>
2023-07-25 08:43:04 -0500

    updated trimol to include logical flags from autochem to be used in reaction rates per #2

--- src/trimolecular-reactions.jl
+++ src/trimolecular-reactions.jl
@@ -19,6 +19,15 @@ struct TrimolecularReaction{T0<:Integer, T1<:AbstractString, T2<:AbstractString,
     c3::T4
     c4::T4
     c5::T4
+    contains_m::Bool
+    contains_ClCO::Bool
+    contains_ClCO3::Bool
+    contains_COCl2::Bool
+    contains_APINENE::Bool
+    contains_BPINENE::Bool
+    contains_ClCO_and_ClCO3::Bool
+    contains_ClCO_and_COCl2::Bool
+    contains_low_term::Bool
 end
 
 
@@ -43,31 +52,49 @@ TrimolecularReaction(rdict::Dict) = TrimolecularReaction(
     rdict["c2"],
     rdict["c3"],
     rdict["c4"],
-    rdict["c5"]
+    rdict["c5"],
+    rdict["contains_m"],
+    rdict["contains_ClCO"],
+    rdict["contains_ClCO3"],
+    rdict["contains_COCl2"],
+    rdict["contains_APINENE"],
+    rdict["contains_BPINENE"],
+    rdict["contains_ClCO_and_ClCO3"],
+    rdict["contains_ClCO_and_COCl2"],
+    rdict["contains_low_term"]
 )
 
 # define how to convert Bimol reaction into JSON for parsing
 JSON.lower(r::TrimolecularReaction) = (;
-                                      idx=r.idx,
-                                      source=r.source,
-                                      reactants=r.reactants,
-                                      products=r.products,
-                                      prod_stoich=r.prod_stoich,
-                                      a1=r.a1,
-                                      a2=r.a2,
-                                      a3=r.a3,
-                                      a4=r.a4,
-                                      a5=r.a5,
-                                      b1=r.b1,
-                                      b2=r.b2,
-                                      b3=r.b3,
-                                      b4=r.b4,
-                                      b5=r.b5,
-                                      c1=r.c1,
-                                      c2=r.c2,
-                                      c3=r.c3,
-                                      c4=r.c4,
-                                      c5=r.c5
+                                       idx=r.idx,
+                                       source=r.source,
+                                       reactants=r.reactants,
+                                       products=r.products,
+                                       prod_stoich=r.prod_stoich,
+                                       a1=r.a1,
+                                       a2=r.a2,
+                                       a3=r.a3,
+                                       a4=r.a4,
+                                       a5=r.a5,
+                                       b1=r.b1,
+                                       b2=r.b2,
+                                       b3=r.b3,
+                                       b4=r.b4,
+                                       b5=r.b5,
+                                       c1=r.c1,
+                                       c2=r.c2,
+                                       c3=r.c3,
+                                       c4=r.c4,
+                                       c5=r.c5,
+                                       contains_m=r.contains_m,
+                                       contains_ClCO=r.contains_ClCO,
+                                       contains_ClCO3=r.contains_ClCO3,
+                                       contains_COCl2=r.contains_COCl2,
+                                       contains_APINENE=r.contains_APINENE,
+                                       contains_BPINENE=r.contains_BPINENE,
+                                       contains_ClCO_and_ClCO3=r.contains_ClCO_and_ClCO3,
+                                       contains_ClCO_and_COCl2=r.contains_ClCO_and_COCl2,
+                                       contains_low_term=r.contains_low_term
                                       )
 
 
@@ -100,13 +127,18 @@ function parse_trimol_d(path)
             @assert length(coeffs_b) == 5
             @assert length(coeffs_c) == 5
 
+
+            reactants = [String(reactant) for reactant ∈ rxn_data[2,2:end] if reactant != ""]
+            products = [String(product) for product ∈ rxn_data[3,2:end] if product != ""]
+
+
             push!(
                 rxns,
                 TrimolecularReaction(
                     rxn_data[1,1],
                     String(rxn_data[1,2]),
-                    [String(reactant) for reactant ∈ rxn_data[2,2:end] if reactant != ""],
-                    [String(product) for product ∈ rxn_data[3,2:end] if product != ""],
+                    reactants,
+                    products,
                     [stoich for stoich ∈ rxn_data[4,2:end] if stoich != ""],
                     coeffs_a[1],
                     coeffs_a[2],
@@ -123,6 +155,15 @@ function parse_trimol_d(path)
                     coeffs_c[3],
                     coeffs_c[4],
                     coeffs_c[5],
+                    any(reactants .== "m"),
+                    any(reactants .== "ClCO"),
+                    any(reactants .== "ClCO3"),
+                    any(reactants .== "COCl2"),
+                    any(reactants .== "APINENE"),
+                    any(reactants .== "BPINENE"),
+                    any(reactants .== "ClCO") && any(reactants .== "ClCO3"),
+                    any(reactants .== "ClCO") && any(reactants .== "COCl2"),
+                    all(coeffs_b .== 0.0)
                 )
             )
         catch e
