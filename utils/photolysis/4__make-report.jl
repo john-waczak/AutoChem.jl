3c5165ba3f86a92a7547a08f9449efa9a9e0d0cf
John Waczak <john.louis.waczak@gmail.com>
2023-08-22 12:50:55 -0500

    updated to include report for quantum yields

--- utils/photolysis/4__make-report.jl
+++ utils/photolysis/4__make-report.jl
@@ -41,8 +41,9 @@ open(outpath_σ, "w") do f
 
             println(f, "# $(speciesname) ($(species))\n")
 
-            fig = Figure();
+            fig = Figure(resolution=(1200, 500));
             ax = Axis(fig[1,1], xlabel="λ (nm)", ylabel="σ (cm²)", title="$(df.formula[1])");
+            ax2 = Axis(fig[1,2], xlabel="λ (nm)", ylabel="log10(σ)", title="$(df.formula[1])");
             scatters = []
             authors = []
             for row ∈ eachrow(df)
@@ -55,14 +56,18 @@ open(outpath_σ, "w") do f
                 println(f, "\n")
 
                 df_σ = CSV.read(joinpath(basepath, row.fname), DataFrame)
+
+                df_σ.σ[df_σ.σ .≤ 0.0] .= 0.0;
+
                 s = scatter!(ax, df_σ.λ, df_σ.σ)
+                scatter!(ax2, df_σ.λ, log10.(df_σ.σ))
                 push!(authors, row.author_year * " T=$(row.T1)")
                 push!(scatters, s)
             end
             leg = Legend(
-                fig[1, 2],
+                fig[1, 3],
                 scatters,
-                authors
+                authors,
             )
 
             save(joinpath(outpath_figures, species*"_σ.png"), fig)
@@ -75,6 +80,62 @@ open(outpath_σ, "w") do f
     end
 end
 
+
+
+# load a sample folder for cross-sections:
+Φ_paths = []
+for (root, dirs, files) ∈ walkdir(basepath_Φ)
+    for file ∈ files
+        if endswith("summary.csv", file)
+            push!(Φ_paths, joinpath(root, file))
+        end
+    end
+end
+
+
+open(outpath_Φ, "w") do f
+    @showprogress for Φ_path ∈ Φ_paths
+        try
+            df = CSV.read(Φ_path, DataFrame)
+
+            rxn = df.reaction[1]
+            rxnname = df.name[1]
+
+            println(f, "# $(rxnname) \n")
+
+            fig = Figure();
+            ax = Axis(fig[1,1], xlabel="λ (nm)", ylabel="Φ ∈ [0,1]", title="$(df.reaction[1])");
+            scatters = []
+            authors = []
+            for row ∈ eachrow(df)
+                println(f, "author:\t$(row.author_year)\n")
+                println(f, "- source:\t$(row.download_url)")
+                println(f, "- comments:\t$(row.comments)")
+                println(f, "- doi:\t$(row.doi)")
+                println(f, "- fname:\t$(row.fname)")
+                println(f, "- temperature:\t$(row.T1)")
+                println(f, "\n")
+
+                df_Φ = CSV.read(joinpath(basepath, row.fname), DataFrame)
+                s = scatter!(ax, df_Φ.λ, df_Φ.Φ)
+                push!(authors, row.author_year * " T=$(row.T1)")
+                push!(scatters, s)
+            end
+            leg = Legend(
+                fig[1, 2],
+                scatters,
+                authors,
+            )
+
+            save(joinpath(outpath_figures, rxnname*"_Φ.png"), fig)
+            figpath = joinpath("./figures", rxnname*"_σ.png")
+
+            println(f, "![]($(figpath))\n")
+        catch e
+            println(e)
+        end
+    end
+end
 
 
 # using LaTeXStrings
