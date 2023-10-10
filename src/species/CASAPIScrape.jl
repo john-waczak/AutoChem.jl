47c2d6e0458f717506a3116d1cfe3327c6e48b2e
Jack Sadler <jacksadler1592@yahoo.com>
2023-10-10 16:55:37 -0500

    Update on CAS API

--- src/species/CASAPIScrape.jl
+++ src/species/CASAPIScrape.jl
@@ -1,16 +1,44 @@
 CASAPIScrape.jl
 
+using CSV
+using DataFrames
 using HTTP
+using JSON
+
+msl = "/Users/jsadler/Desktop/Julia/AutoChem.jl/src/species/master_species_list.csv"
+csv_msl = CSV.File(msl)
+
+msl_df = CSV.read("/Users/jsadler/Desktop/Julia/AutoChem.jl/src/species/master_species_list.csv", DataFrame)
 
 url = "https://commonchemistry.cas.org/api"
 
 r = HTTP.get(url)
 
+molecule_name = "Formaldehyde"
+query_url = "$url/search?q=$molecule_name"
+response = HTTP.get(query_url)
+
 using Gumbo
 
-h = parsehtml(String(r.body))
+if response.status == 200
+else
+    println("Error: HTTP request failed with status code {response.status}")
+end
 
-h.root[1]
+parsed_html = parsehtml(String(response.body))
 
 using Cascadia
+
+links = eachmatch(sel"your_css_selector_here", parsed_html.root)
+
+for link in links
+    link_text = text(link)
+    link_href = get(link.attributes, "href", "")
+
+    println("Link Text: $link_text")
+    println("Link Href: $link_href")
+end
+
+
+
 
